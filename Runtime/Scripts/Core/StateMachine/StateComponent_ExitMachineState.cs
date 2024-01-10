eeb61f584782b55d9c9a30d608be226255b59925
Morgan Hoarau <43406539+Nobuna-no@users.noreply.github.com>
2024-01-10 12:06:54 +0000

    Update StateComponent_ExitMachineState.cs

--- Runtime/Scripts/Core/StateMachine/StateComponent_ExitMachineState.cs
+++ Runtime/Scripts/Core/StateMachine/StateComponent_ExitMachineState.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace NobunAtelier
@@ -9,12 +10,26 @@ namespace NobunAtelier
         private enum ExitStateMachineTrigger
         {
             Manual,
+            AfterDelay,
             OnStateEnter,
         }
 
         [Header("State Machine Exit")]
         [SerializeField]
         private ExitStateMachineTrigger m_trigger = ExitStateMachineTrigger.OnStateEnter;
+        
+        [SerializeField, ShowIf("IsDelayMode"), Min(0)]
+        private float m_delayInSeconds = 1f;
+
+        private float m_currentDelay = 0f;
+
+        private bool IsDelayMode => m_trigger == ExitStateMachineTrigger.AfterDelay;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            enabled = false;
+        }
 
         public override void Enter()
         {
@@ -23,11 +38,27 @@ namespace NobunAtelier
             {
                 ParentStateMachine.ExitStateMachine();
             }
+
+            if (m_trigger == ExitStateMachineTrigger.AfterDelay)
+            {
+                m_currentDelay = m_delayInSeconds;
+                enabled = true;
+            }
         }
 
         public void ExitStateMachine()
         {
             ParentStateMachine.ExitStateMachine();
+        }
+
+        private void Update()
+        {
+            m_currentDelay -= Time.deltaTime;
+            if (m_currentDelay <= 0)
+            {
+                ParentStateMachine.ExitStateMachine();
+                enabled = false;
+            }
         }
     }
 }
