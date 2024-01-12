bacfd953fe70f1850b7b7e761dc05bc214ed3620
Morgan Hoarau <43406539+Nobuna-no@users.noreply.github.com>
2024-01-12 16:02:31 +0000

    Adds empty sampleTest scripts for warning log

---
+++ Tests/Scripts/Editor/SampleTestScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace NobunAtelier.Tests.Editor
+{
+    public class SampleTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SampleTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator SampleTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
