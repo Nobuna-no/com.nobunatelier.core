10fc602a4609c38ef54d4c029c5e3a2ec814ffc6
Morgan Hoarau <43406539+Nobuna-no@users.noreply.github.com>
2024-04-08 23:50:28 +0100

    Adds hold action and buffer to player controller module

--- Runtime/Scripts/Controller/Modules/PlayerController/PlayerControllerAttack.cs
+++ Runtime/Scripts/Controller/Modules/PlayerController/PlayerControllerAttack.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -11,9 +12,16 @@ namespace NobunAtelier
 
         public override void EnableModuleInput(PlayerInput playerInput, InputActionMap activeActionMap)
         {
+            bool needUpdate = false;
             foreach (var action in m_actions)
             {
                 action.EnableAction(activeActionMap);
+                needUpdate |= action.CanBeHoldAction;
+            }
+
+            if (needUpdate)
+            {
+                StartCoroutine(UpdateRoutine());
             }
         }
 
@@ -23,6 +31,28 @@ namespace NobunAtelier
             {
                 action.DisableAction(activeActionMap);
             }
+
+            // If the coroutine hasn't started, will do nothing.
+            StopCoroutine(UpdateRoutine());
+        }
+
+        private IEnumerator UpdateRoutine()
+        {
+            while (true)
+            {
+                foreach (var action in m_actions)
+                {
+                    if (!action.NeedUpdate)
+                    {
+                        continue;
+                    }
+
+                    action.UpdateAction(Time.unscaledDeltaTime);
+                }
+
+                // In case user use slowmo style, we don't want player input to be impacted.
+                yield return new WaitForSecondsRealtime(Time.unscaledDeltaTime);
+            }
         }
 
         [System.Serializable]
@@ -31,8 +61,23 @@ namespace NobunAtelier
             [SerializeField] private string m_actionName = "";
             [SerializeField] private UnityEvent m_onActionPerformed;
             [SerializeField] private UnityEvent m_onActionCancelled;
+            /// <summary>
+            /// OnActionHold is going to be called instead of OnActionPerformed.
+            /// If action is canceled before the minimum hold threshold, OnActionPerformed will be called instead.
+            /// </summary>
+            [SerializeField]
+            private UnityEvent m_onActionHold;
+            [SerializeField, AllowNesting, ShowIf("CanBeHoldAction"), Range(0.01f, 1f)]
+            private float m_holdTreshold = 0.1f;
+            [SerializeField, AllowNesting, ShowIf("CanBeHoldAction")]
+            private bool m_raiseActionPerformedOnHoldCancelled = true;
+
+            public bool CanBeHoldAction => m_onActionHold.GetPersistentEventCount() > 0;
+
+            public bool NeedUpdate { get; private set; } = false;
 
             private InputAction m_inputAction;
+            private float m_holdDuration = 0f;
 
             public void EnableAction(InputActionMap map)
             {
@@ -53,13 +98,41 @@ namespace NobunAtelier
                 m_inputAction.canceled -= Cancel;
             }
 
+            public void UpdateAction(float deltaTime)
+            {
+                m_holdDuration += deltaTime;
+                if (m_holdDuration >= m_holdTreshold)
+                {
+                    m_onActionHold?.Invoke();
+                    NeedUpdate = false;
+                }
+            }
+
             private void Perform(InputAction.CallbackContext obj)
             {
+                if (CanBeHoldAction)
+                {
+                    m_holdDuration = 0;
+                    NeedUpdate = true;
+                    return;
+                }
+
                 m_onActionPerformed?.Invoke();
             }
 
             private void Cancel(InputAction.CallbackContext obj)
             {
+                if (CanBeHoldAction)
+                {
+                    NeedUpdate = false;
+                    if (m_holdDuration < m_holdTreshold && m_raiseActionPerformedOnHoldCancelled)
+                    {
+                        // If an hold action was in progress and has been canceled,
+                        // it is considered as a normal Perform.
+                        m_onActionPerformed?.Invoke();
+                    }
+                }
+
                 m_onActionCancelled?.Invoke();
             }
         }
