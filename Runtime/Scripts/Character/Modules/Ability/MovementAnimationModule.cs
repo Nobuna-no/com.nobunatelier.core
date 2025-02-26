207a2e81b9ca8282619e2943690c25b1de54e587
Morgan Hoarau <43406539+Nobuna-no@users.noreply.github.com>
2025-02-26 09:13:50 +0000

    Update MovementAnimationModule.cs

--- Runtime/Scripts/Character/Modules/Ability/MovementAnimationModule.cs
+++ Runtime/Scripts/Character/Modules/Ability/MovementAnimationModule.cs
@@ -7,13 +7,16 @@ namespace NobunAtelier
     {
         [SerializeField, AnimatorParam("m_animator")]
         private string m_moveSpeedFloatName;
-
+        [SerializeField, Min(0f)] private float m_motionDamping = 1f;
+        private float m_smoothSpeed = 0;
         protected override void OnAbilityUpdate(float deltaTime)
         {
             var movement = ModuleOwner.GetMoveVector();
             movement.y = 0;
 
-            Animator.SetFloat(m_moveSpeedFloatName, movement.sqrMagnitude);
+            m_smoothSpeed = Mathf.Lerp(m_smoothSpeed, movement.sqrMagnitude, deltaTime * m_motionDamping);
+
+            Animator.SetFloat(m_moveSpeedFloatName, m_smoothSpeed);
         }
     }
 }
