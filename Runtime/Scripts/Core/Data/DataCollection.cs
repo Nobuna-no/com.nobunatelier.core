f5cd1477c3be181a1eb1975217b4fde2a6e2a4db
Morgan Hoarau <43406539+Nobuna-no@users.noreply.github.com>
2025-04-21 19:46:56 +0100

    Refactor DataDef/Col

--- Runtime/Scripts/Core/Data/DataCollection.cs
+++ Runtime/Scripts/Core/Data/DataCollection.cs
@@ -1,72 +1,194 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using System;
 
 #if UNITY_EDITOR
-
 using UnityEditor;
-
 #endif
 
 namespace NobunAtelier
 {
+    /// <summary>
+    /// Base class for all data collections in the NobunAtelier framework.
+    /// This class provides the foundation for a data-driven architecture using ScriptableObjects.
+    /// It enables designers to create and manage collections of data definitions that can be used
+    /// across different systems like factories, audio management, and more.
+    /// </summary>
     public abstract class DataCollection : ScriptableObject
     {
-
+        /// <summary>
+        /// Gets a random definition from the collection.
+        /// </summary>
+        /// <returns>A random DataDefinition from the collection, or null if the collection is empty.</returns>
         public abstract DataDefinition GetRandomDefinition();
 
 #if UNITY_EDITOR
+        /// <summary>
+        /// Gets all data definitions in this collection for editor purposes.
+        /// </summary>
         public abstract DataDefinition[] EditorDataDefinitions { get; }
 
+        /// <summary>
+        /// Gets the type of definitions stored in this collection.
+        /// </summary>
+        /// <returns>The Type of the DataDefinition subclass used by this collection.</returns>
         public abstract Type GetDefinitionType();
 
+        /// <summary>
+        /// Adds a new definition asset to the collection.
+        /// </summary>
+        /// <param name="asset">The DataDefinition asset to add.</param>
         public abstract void AddDefinitionAssetToCollection(DataDefinition asset);
 
+        /// <summary>
+        /// Creates a new definition in the collection.
+        /// </summary>
+        /// <param name="duplicateFromLastData">If true, duplicates the last definition's properties.</param>
         public abstract void CreateDefinition(bool duplicateFromLastData = true);
 
+        /// <summary>
+        /// Saves the collection and its definitions to disk.
+        /// </summary>
         public abstract void SaveCollection();
 
+        /// <summary>
+        /// Gets a definition by its name.
+        /// </summary>
+        /// <param name="name">The name of the definition to find.</param>
+        /// <returns>The found DataDefinition, or null if not found.</returns>
         public abstract DataDefinition GetDefinition(string name);
 
+        /// <summary>
+        /// Gets a definition by its name, or creates a new one if it doesn't exist.
+        /// </summary>
+        /// <param name="name">The name of the definition to find or create.</param>
+        /// <returns>The found or newly created DataDefinition.</returns>
         public abstract DataDefinition GetOrCreateDefinition(string name);
 
+        /// <summary>
+        /// Deletes a definition from the collection.
+        /// </summary>
+        /// <param name="index">The index of the definition to delete.</param>
+        /// <param name="name">The name of the definition to delete (for validation).</param>
         public abstract void DeleteDefinition(int index, string name);
 
+        /// <summary>
+        /// Renames a definition in the collection.
+        /// </summary>
+        /// <param name="index">The index of the definition to rename.</param>
+        /// <param name="name">The new name for the definition.</param>
         public abstract void RenameDefinition(int index, string name);
 
+        /// <summary>
+        /// Moves a definition to a new position in the collection.
+        /// </summary>
+        /// <param name="oldIndex">The current index of the definition.</param>
+        /// <param name="newIndex">The new index for the definition.</param>
         public abstract void MoveDefinition(int oldIndex, int newIndex);
 
+        /// <summary>
+        /// Forces a definition to be set at a specific index in the collection.
+        /// This is useful for editor operations and data migration.
+        /// </summary>
+        /// <param name="index">The index where to set the definition.</param>
+        /// <param name="data">The DataDefinition to set.</param>
         public abstract void ForceSetDataDefinition(int index, DataDefinition data);
-
 #endif
     }
 
-    public abstract class DataCollection<T> : DataCollection
+    /// <summary>
+    /// Generic base class for data collections that provides type-safe access to definitions.
+    /// This class implements the core functionality for managing collections of specific DataDefinition types.
+    /// </summary>
+    /// <typeparam name="T">The type of DataDefinition stored in this collection. Must inherit from DataDefinition.</typeparam>
+    public abstract class DataCollection<T> : DataCollection, IEnumerable<T>
         where T : DataDefinition
     {
+        [FormerlySerializedAs("m_dataDefinitions")]
         [SerializeField]
-        protected List<T> m_dataDefinitions = new List<T>();
+        protected internal List<T> m_DataDefinitions = new List<T>();
 
-        public IReadOnlyList<T> Definitions => m_dataDefinitions;
+        /// <summary>
+        /// Gets a read-only list of all definitions in this collection.
+        /// </summary>
+        public IReadOnlyList<T> Definitions => m_DataDefinitions;
 
+        /// <summary>
+        /// Gets the number of definitions in this collection.
+        /// </summary>
+        public int Count => m_DataDefinitions.Count;
+
+        /// <summary>
+        /// Gets the definition at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the definition to get.</param>
+        /// <returns>The definition at the specified index.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">Thrown when index is less than 0 or greater than or equal to Count.</exception>
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= m_DataDefinitions.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), "Index was out of range. Must be non-negative and less than the size of the collection.");
+                }
+                return m_DataDefinitions[index];
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return m_DataDefinitions.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets a random definition from the collection.
+        /// </summary>
+        /// <returns>A random T from the collection, or null if the collection is empty.</returns>
         public override DataDefinition GetRandomDefinition()
         {
-            if (m_dataDefinitions.Count == 0)
+            if (m_DataDefinitions.Count == 0)
             {
                 return null;
             }
 
-            return m_dataDefinitions[UnityEngine.Random.Range(0, m_dataDefinitions.Count)];
+            return m_DataDefinitions[UnityEngine.Random.Range(0, m_DataDefinitions.Count)];
         }
 
 #if UNITY_EDITOR
-        public override DataDefinition[] EditorDataDefinitions => m_dataDefinitions.ToArray();
+        /// <summary>
+        /// Gets all data definitions in this collection for editor purposes.
+        /// </summary>
+        public override DataDefinition[] EditorDataDefinitions => m_DataDefinitions.ToArray();
 
+        /// <summary>
+        /// Gets the type of definitions stored in this collection.
+        /// </summary>
+        /// <returns>The Type of T used by this collection.</returns>
         public override Type GetDefinitionType()
         {
             return typeof(T);
         }
 
+        /// <summary>
+        /// Adds a new definition asset to the collection.
+        /// Validates the type and name before adding.
+        /// </summary>
+        /// <param name="asset">The DataDefinition asset to add.</param>
         public override void AddDefinitionAssetToCollection(DataDefinition asset)
         {
             if (!asset.GetType().IsAssignableFrom(typeof(T)))
@@ -75,7 +197,7 @@ namespace NobunAtelier
                 return;
             }
 
-            foreach (var dataDefinition in m_dataDefinitions)
+            foreach (var dataDefinition in m_DataDefinitions)
             {
                 if (dataDefinition.name == asset.name)
                 {
@@ -95,6 +217,11 @@ namespace NobunAtelier
             Selection.activeObject = this;
         }
 
+        /// <summary>
+        /// Clones an asset and adds it to the collection.
+        /// </summary>
+        /// <param name="sourceAsset">The source asset to clone.</param>
+        /// <param name="name">The name for the new asset.</param>
         private void CloneAsset(DataDefinition sourceAsset, string name)
         {
             T newAsset = ScriptableObject.CreateInstance<T>();
@@ -119,29 +246,33 @@ namespace NobunAtelier
             // Apply changes and save the new asset
             newSerializedObject.ApplyModifiedProperties();
 
-            m_dataDefinitions.Add(newAsset);
+            m_DataDefinitions.Add(newAsset);
 
             AssetDatabase.AddObjectToAsset(newAsset, this);
             EditorUtility.SetDirty(newAsset);
             AssetDatabase.SaveAssetIfDirty(newAsset);
         }
 
+        /// <summary>
+        /// Creates a new definition in the collection.
+        /// </summary>
+        /// <param name="duplicateFromLastData">If true, duplicates the last definition's properties.</param>
         public override sealed void CreateDefinition(bool duplicateFromLastData = true)
         {
-            int containerCount = m_dataDefinitions.Count;
+            int containerCount = m_DataDefinitions.Count;
 
             if (!duplicateFromLastData || containerCount == 0)
             {
                 T data = ScriptableObject.CreateInstance<T>();
                 data.name = $"{this.name}_data_{containerCount}";
-                m_dataDefinitions.Add(data);
+                m_DataDefinitions.Add(data);
                 AssetDatabase.AddObjectToAsset(data, this);
                 EditorUtility.SetDirty(data);
                 AssetDatabase.SaveAssetIfDirty(data);
             }
             else
             {
-                var latestData = m_dataDefinitions[containerCount - 1];
+                var latestData = m_DataDefinitions[containerCount - 1];
                 CloneAsset(latestData, $"{latestData.name.Split("_$")[0]}_${GUID.Generate().ToString()}");
             }
 
@@ -150,9 +281,14 @@ namespace NobunAtelier
             AssetDatabase.Refresh();
         }
 
+        /// <summary>
+        /// Gets a definition by its name.
+        /// </summary>
+        /// <param name="name">The name of the definition to find.</param>
+        /// <returns>The found T, or null if not found.</returns>
         public override sealed DataDefinition GetDefinition(string name)
         {
-            foreach (var dataDefinition in m_dataDefinitions)
+            foreach (var dataDefinition in m_DataDefinitions)
             {
                 if (dataDefinition.name == name)
                 {
@@ -163,9 +299,14 @@ namespace NobunAtelier
             return null;
         }
 
+        /// <summary>
+        /// Gets a definition by its name, or creates a new one if it doesn't exist.
+        /// </summary>
+        /// <param name="name">The name of the definition to find or create.</param>
+        /// <returns>The found or newly created T.</returns>
         public override sealed DataDefinition GetOrCreateDefinition(string name)
         {
-            foreach (var dataDefinition in m_dataDefinitions)
+            foreach (var dataDefinition in m_DataDefinitions)
             {
                 if (dataDefinition.name == name)
                 {
@@ -175,7 +316,7 @@ namespace NobunAtelier
 
             T data = ScriptableObject.CreateInstance<T>();
             data.name = name;
-            m_dataDefinitions.Add(data);
+            m_DataDefinitions.Add(data);
 
             AssetDatabase.AddObjectToAsset(data, this);
             EditorUtility.SetDirty(data);
@@ -187,48 +328,66 @@ namespace NobunAtelier
             return data;
         }
 
+        /// <summary>
+        /// Deletes a definition from the collection.
+        /// </summary>
+        /// <param name="index">The index of the definition to delete.</param>
+        /// <param name="name">The name of the definition to delete (for validation).</param>
         public override sealed void DeleteDefinition(int index, string name)
         {
-            if (m_dataDefinitions.Count <= index || m_dataDefinitions[index].name != name)
+            if (m_DataDefinitions.Count <= index || m_DataDefinitions[index].name != name)
             {
                 return;
             }
 
-            AssetDatabase.RemoveObjectFromAsset(m_dataDefinitions[index]);
-            m_dataDefinitions.RemoveAt(index);
+            AssetDatabase.RemoveObjectFromAsset(m_DataDefinitions[index]);
+            m_DataDefinitions.RemoveAt(index);
 
             EditorUtility.SetDirty(this);
             AssetDatabase.SaveAssetIfDirty(this);
             AssetDatabase.Refresh();
         }
 
+        /// <summary>
+        /// Renames a definition in the collection.
+        /// </summary>
+        /// <param name="index">The index of the definition to rename.</param>
+        /// <param name="name">The new name for the definition.</param>
         public override sealed void RenameDefinition(int index, string name)
         {
-            if (m_dataDefinitions.Count <= index)
+            if (m_DataDefinitions.Count <= index)
             {
                 return;
             }
 
-            m_dataDefinitions[index].name = name;
+            m_DataDefinitions[index].name = name;
 
-            EditorUtility.SetDirty(m_dataDefinitions[index]);
-            AssetDatabase.SaveAssetIfDirty(m_dataDefinitions[index]);
+            EditorUtility.SetDirty(m_DataDefinitions[index]);
+            AssetDatabase.SaveAssetIfDirty(m_DataDefinitions[index]);
             AssetDatabase.Refresh();
         }
 
+        /// <summary>
+        /// Moves a definition to a new position in the collection.
+        /// </summary>
+        /// <param name="previousIndex">The current index of the definition.</param>
+        /// <param name="newIndex">The new index for the definition.</param>
         public override void MoveDefinition(int previousIndex, int newIndex)
         {
-            Debug.Assert(previousIndex < m_dataDefinitions.Count);
-            Debug.Assert(newIndex < m_dataDefinitions.Count);
-            var dataToMove = m_dataDefinitions[previousIndex];
-            m_dataDefinitions.RemoveAt(previousIndex);
-            m_dataDefinitions.Insert(newIndex, dataToMove);
+            Debug.Assert(previousIndex < m_DataDefinitions.Count);
+            Debug.Assert(newIndex < m_DataDefinitions.Count);
+            var dataToMove = m_DataDefinitions[previousIndex];
+            m_DataDefinitions.RemoveAt(previousIndex);
+            m_DataDefinitions.Insert(newIndex, dataToMove);
 
             EditorUtility.SetDirty(this);
             AssetDatabase.SaveAssetIfDirty(this);
             AssetDatabase.Refresh();
         }
 
+        /// <summary>
+        /// Saves the collection and its definitions to disk.
+        /// </summary>
         public override void SaveCollection()
         {
             EditorUtility.SetDirty(this);
@@ -236,18 +395,24 @@ namespace NobunAtelier
             AssetDatabase.Refresh();
         }
 
+        /// <summary>
+        /// Forces a definition to be set at a specific index in the collection.
+        /// This is useful for editor operations and data migration.
+        /// </summary>
+        /// <param name="index">The index where to set the definition.</param>
+        /// <param name="data">The DataDefinition to set.</param>
         public override void ForceSetDataDefinition(int index, DataDefinition data)
         {
             var typedData = data as T;
             Debug.Assert(typedData != null, $"{this.name}: Trying to assigned {data.name} to the collection, but is not of the right type {typeof(T)}");
-            Debug.Assert(index < m_dataDefinitions.Count);
+            Debug.Assert(index < m_DataDefinitions.Count);
 
             if (!typedData)
             {
                 return;
             }
 
-            m_dataDefinitions[index] = data as T;
+            m_DataDefinitions[index] = data as T;
 
             EditorUtility.SetDirty(this);
             AssetDatabase.SaveAssetIfDirty(this);
--- Runtime/Scripts/Core/Data/DataDefinition.cs
+++ Runtime/Scripts/Core/Data/DataDefinition.cs
@@ -1,28 +1,40 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace NobunAtelier
 {
+    /// <summary>
+    /// Base class for all data definitions. This class is used to define the data structure for a specific type of data.
+    /// </summary>
     public abstract class DataDefinition : ScriptableObject
     {
     }
 
+    /// <summary>
+    /// Generic base class for data definitions.
+    /// This class is used to define the data structure for a specific type of data.
+    /// </summary>
     public abstract class DataDefinition<T> : DataDefinition
     {
-        public T Data => m_data;
-
+        [FormerlySerializedAs("m_data")]
         [SerializeField]
-        private T m_data;
+        private T m_Data;
+
+        public T Data => m_Data;
     }
 
+    /// <summary>
+    /// Base class for all data instances. This class can be use to separate logic from data.
+    /// </summary>
     public abstract class DataInstance<T>
         where T : DataDefinition
     {
-        private T m_data;
-        public T Data => m_data;
+        private T m_Data;
+        public T Data => m_Data;
 
         public DataInstance(T data)
         {
-            m_data = data;
+            m_Data = data;
         }
     }
 }
