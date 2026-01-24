9265d80173b91edd521e73ae75c1cf2f2652e667
Morgan Hoarau <43406539+Nobuna-no@users.noreply.github.com>
2026-01-24 18:45:07 +0000

    Update LoadableComponent.cs

--- Runtime/Scripts/Core/ResourceManagement/LoadableComponent.cs
+++ Runtime/Scripts/Core/ResourceManagement/LoadableComponent.cs
@@ -52,65 +52,56 @@ namespace NobunAtelier
         where AssetRefT : LoadableComponent<T>
         where PoolT : LoadableComponentPool<T, AssetRefT>
     {
-        private static Dictionary<string, PoolT> s_AddressableFactoriesMap = null;
-        private static GameObject s_AtelierFactoryGao;
+        private static Dictionary<string, PoolT> s_AddressableFactoryMap = null;
+        private static GameObject s_FactoryGao;
+        private static string k_FactoryName = typeof(LoadableComponentPoolFactory<T, AssetRefT, PoolT>).Name;
 
         [SerializeField]
-        private AssetReferenceGameObject m_ObjectPoolPrefab = null;
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void ResetStaticState()
-        {
-            if (s_AtelierFactoryGao != null)
-            {
-                Object.Destroy(s_AtelierFactoryGao);
-                s_AtelierFactoryGao = null;
-            }
-            
-            foreach (var factory in s_AddressableFactoriesMap)
-            {
-                factory.Value.ObjectPool.Clear();
-            }
-
-            s_AddressableFactoriesMap = null;
-        }
+        private AssetReferenceGameObject objectPoolPrefab = null;
 
         private static Dictionary<string, PoolT> Instance
         {
             get
             {
-                if (s_AddressableFactoriesMap == null)
+                if (s_AddressableFactoryMap == null)
                 {
-                    s_AddressableFactoriesMap = new Dictionary<string, PoolT>();
-                    s_AtelierFactoryGao = new GameObject($"Atelier Factory ({typeof(T).Name})");
+                    s_AddressableFactoryMap = new Dictionary<string, PoolT>();
+                    s_FactoryGao = new GameObject($"Atelier Factory ({typeof(T).Name})");
                 }
 
-                return s_AddressableFactoriesMap;
+                return s_AddressableFactoryMap;
             }
         }
 
-        public static void CreateFactory(AssetRefT assetRef, int initialReserve = 1)
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void ResetOnInitialization()
+        {
+            s_AddressableFactoryMap = null;
+            s_FactoryGao = null;
+        }
+
+        public static void CreateFactory(AssetRefT assetRef, int initialReserve = 3)
         {
             if (string.IsNullOrEmpty(assetRef.AssetGUID))
             {
-                Debug.LogError($"Atelier Factory ({typeof(T).Name}): trying to create a factory with an invalid asset reference.");
+                Debug.LogError($"{k_FactoryName}: trying to create a factory with an invalid asset reference.");
                 return;
             }
 
             if (!Instance.ContainsKey(assetRef.AssetGUID))
             {
-                Instance.Add(assetRef.AssetGUID, s_AtelierFactoryGao.AddComponent<PoolT>());
-                Instance[assetRef.AssetGUID].SetInitialSize(initialReserve);
-                Instance[assetRef.AssetGUID].SetAssetReference(assetRef);
-                Instance[assetRef.AssetGUID].ResetPool();
+                Instance.Add(assetRef.AssetGUID, s_FactoryGao.AddComponent<PoolT>());
             }
+
+            Instance[assetRef.AssetGUID].SetAssetReference(assetRef);
+            Instance[assetRef.AssetGUID].ResetPool(initialReserve);
         }
 
         public static T Get(AssetRefT assetRef)
         {
             if (string.IsNullOrEmpty(assetRef.AssetGUID))
             {
-                Debug.LogError($"Atelier Factory ({typeof(T).Name}): trying to get a product but asset reference is invalid.");
+                Debug.LogError($"{k_FactoryName}: trying to get a product but asset reference is invalid.");
                 return null;
             }
 
@@ -124,9 +115,15 @@ namespace NobunAtelier
 
         public static void Release(AssetRefT assetRef, T product)
         {
+            if (assetRef == null)
+            {
+                Debug.LogError($"{k_FactoryName}: Trying to release product '{product.name}' using unknown asset reference.");
+                return;
+            }
+
             if (!Instance.ContainsKey(assetRef.AssetGUID))
             {
-                Debug.LogError($"AddressablePrefabAtelierFactory: Trying to release product '{product.name}' using unknown '{assetRef}'. This may cause a memory leak.");
+                Debug.LogError($"{k_FactoryName}: Trying to release product '{product.name}' using unknown '{assetRef}'. This may cause a memory leak.");
                 return;
             }
 
@@ -136,14 +133,14 @@ namespace NobunAtelier
         // This is going to be used by each individual factory.
         public override void SetAssetReference(AssetRefT assetReference)
         {
-            m_ObjectPoolPrefab = assetReference;
+            objectPoolPrefab = assetReference;
         }
 
         // invoked when creating an item to populate the object pool
         protected override T OnProductCreation()
         {
 #if UNITY_EDITOR
-            AsyncOperationHandle<GameObject> poolHandle = m_ObjectPoolPrefab.InstantiateAsync(Vector3.zero, Quaternion.identity, transform);
+            AsyncOperationHandle<GameObject> poolHandle = objectPoolPrefab.InstantiateAsync(Vector3.zero, Quaternion.identity, transform);
 #else
             AsyncOperationHandle<GameObject> poolHandle = objectPoolPrefab.InstantiateAsync(Vector3.zero, Quaternion.identity);
 #endif
@@ -166,7 +163,7 @@ namespace NobunAtelier
         // invoked when we exceed the maximum number of pooled items (i.e. destroy the pooled object)
         protected override void OnProductDestruction(T product)
         {
-            m_ObjectPoolPrefab.ReleaseInstance(product.gameObject);
+            objectPoolPrefab.ReleaseInstance(product.gameObject);
         }
     }
 }
